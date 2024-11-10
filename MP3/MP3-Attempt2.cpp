acbe860d148b4a5443a191dacf06070a09fac0e8
CMacSwam <c-mccluskey.1@onu.edu>
2024-11-10 11:05:47 -0500

    FinalProject-without comments

--- MP3/MP3-Attempt2.cpp
+++ MP3/MP3-Attempt2.cpp
@@ -19,66 +19,84 @@ void clearBoard(char board[]);
 bool hasThreeInRow(char board[], char playerMark);
 void displayBoard(char board[]);
 void displayGameStats(int ties, int player1Score, int player2Score);
-string getPlayerName();
 bool boardFull(char board[]);
 
 
 
 int main() {
 
-	bool gameOver = false;
-	string player1;
-	string player2;
-	int player1Wins = 0;
-	int player2Wins = 0;
-	int numberOfTies = 0;
+	
+	string player1, player2;
+	int player1Wins = 0, player2Wins = 0, numberOfTies = 0;
 	char board[9] = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-	int player1Input;
+	bool playAgain = true;
+	int currentPlayer = 1;
 	char player1Mark = 'X';
 	char player2Mark = 'O';
-	int player2Input;
 
-	player1 = getPlayerName();
-	player2 = getPlayerName();
+	cout << "Enter PLayer 1 name(X): ";
+	getline(cin, player1);
+	cout << "Enter PLayer 2 name(O): ";
+	getline(cin, player2);
 
-	do {
+	while (playAgain) {
+
+		clearBoard(board);
 		displayBoard(board);
-		player1Input = getPlayerInput(player1, board);
-		placeMarkOnBoard(board, player1Mark, player1Input);
-		displayBoard(board);
-		player2Input = getPlayerInput(player2, board);
-		placeMarkOnBoard(board, player2Mark, player2Input);
-		if (hasThreeInRow(board, player1Mark)) {
-			player1Wins++;
-			clearBoard(board);
-			displayGameStats(numberOfTies, player1Wins, player2Wins);
-		}
-		else if (hasThreeInRow(board, player2Mark)) {
-			player2Wins++;
-			clearBoard(board);
-			displayGameStats(numberOfTies, player1Wins, player2Wins);
-		}
-		else if (boardFull(board)) {
-			numberOfTies++;
-			clearBoard(board);
-			displayGameStats(numberOfTies, player1Wins, player2Wins);
-		}
-		else {
-			continue;
+
+		int moves = 0;
+		bool gameOver = false;
+
+		while (!gameOver && moves < 9) {
+			
+
+			int move = getPlayerInput(player1, board);
+			placeMarkOnBoard(board, player1Mark, move);
+			displayBoard(board);
+			moves++;
+
+			if (hasThreeInRow(board, player1Mark)) {
+				cout << player1 << " wins!" << endl;
+				gameOver = true;
+				player1Wins++;
+				break;
+			}
+			if (moves == 9) {
+				cout << "It's a tie!" << endl;
+				numberOfTies++;
+				break;
+			}
+
+			move = getPlayerInput(player2, board);
+			placeMarkOnBoard(board, player2Mark, move);
+			displayBoard(board);
+			moves++;
+
+			if (hasThreeInRow(board, player2Mark)) {
+				cout << player2 << " wins!" << endl;
+				gameOver = true;
+				player2Wins++;
+			}
+
+			if (moves == 9) {
+				cout << "It's a tie!" << endl;
+				numberOfTies++;
+				break;
+			}
 		}
 
-	} while (!gameOver);
-	
+		displayGameStats(numberOfTies, player1Wins, player2Wins);
+
+		char quit;
+		cout << "Do you want to play again? Enter 'Y' or 'y': ";
+		cin >> quit;
+		playAgain = (quit == 'y' || quit == 'Y');
+	}
+
+	cout << "Thanks for playing!" << endl;
 
 }
 
-string getPlayerName() {
-	string playerName;
-	cout << "Please enter players name: ";
-	cin >> playerName;
-	cout << endl;
-	return playerName;
-}
 
 int getPlayerInput(string playerName, char board[]) {
 
@@ -88,16 +106,20 @@ int getPlayerInput(string playerName, char board[]) {
 
 		cout << endl << playerName << " please enter the location you want to place your mark: ";
 		cin >> location;
-		
+
+		if (cin.fail()) {
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			cout << "Invalid input, please enter a valid number 1-9: ";
+			continue;
+		}
+
 		if (location >= 1 && location <= 9) {
 			if (isLegalMove(board, location)) {
-				if (board[location - 1] == ('X' || 'O')) {
-					cout << "That location is already taken, please enter another location\n";
-				}
-				else {
-					return location - 1;
-					break;
-				}
+				return location - 1;
+			}
+			else {
+				cout << "That move is not legal, please enter another location\n";
 			}
 		}
 		else {
@@ -110,35 +132,20 @@ int getPlayerInput(string playerName, char board[]) {
 
 bool isLegalMove(char board[], int location) {
 
-	if ((board[location] == 'O') || (board[location] == 'X')) {
-		return false;
-	}
-	else; return true;
+	return (board[location - 1] != 'X' && board[location - 1] != 'O');
 
 }
 
 void placeMarkOnBoard(char board[], char playerMark, int location) {
 
-	if (isLegalMove(board, location)) {
-		board[location] = playerMark;
-	}
-	else {
-		while (true) {
-			cout << "Not a legal move, please enter another mark('X' or 'O'): ";
-			cin >> location;
-			if (isLegalMove(board, location)) {
-				board[location] = playerMark;
-				break;
-			}
-		}
-	}
+	board[location] = playerMark;
 
 }
 
 void clearBoard(char board[]) {
 
-	for (int i = 9; i >= 0; i--) {
-		board[i] = i;
+	for (int i = 0; i < 9; i++) {
+		board[i] = '1' + i; //converts integer to corresponding chararacter 
 	}
 
 }
@@ -148,39 +155,26 @@ bool hasThreeInRow(char board[], char playerMark) {
 	//Needs Work
 
 	//possibly add switch statement
-	if ((playerMark == board[0]) && (playerMark == board[1]) && (playerMark == board[2])) {
-		return true;
+	for (int i = 0; i < 9; i += 3) {
+		if (board[i] == playerMark && board[i + 1] == playerMark && board[i + 2] == playerMark) {
+			return true;
+		}
 	}
-	else if ((playerMark == board[3]) && (playerMark == board[4]) && (playerMark == board[5])) {
-		return true;
+	// Check columns
+	for (int i = 0; i < 3; ++i) {
+		if (board[i] == playerMark && board[i + 3] == playerMark && board[i + 6] == playerMark) {
+			return true;
+		}
 	}
-	else if ((playerMark == board[6]) && (playerMark == board[7]) && (playerMark == board[8])) {
-		return true;
-	}
-	else if((playerMark == board[0]) && (playerMark == board[3]) && (playerMark == board[6])) {
-		return true;
-	}
-	else if ((playerMark == board[1]) && (playerMark == board[4]) && (playerMark == board[7])) {
-		return true;
-	}
-	else if ((playerMark == board[2]) && (playerMark == board[5]) && (playerMark == board[8])) {
-		return true;
-	}
-	else if ((playerMark == board[0]) && (playerMark == board[4]) && (playerMark == board[8])) {
-		return true;
-	}
-	else if ((playerMark == board[6]) && (playerMark == board[4]) && (playerMark == board[2])) {
-		return true;
-	}
-	else {
-		return false;
-	}
+	// Check diagonals
+	if (board[0] == playerMark && board[4] == playerMark && board[8] == playerMark) return true;
+	if (board[2] == playerMark && board[4] == playerMark && board[6] == playerMark) return true;
 
+	return false;
 
 }
 
 void displayBoard(char board[]) {
-
 	cout << "\n " << board[6] << " | " << board[7] << " | " << board[8] << endl;
 	cout << "---+---+---\n";
 	cout << " " << board[3] << " | " << board[4] << " | " << board[5] << endl;
@@ -192,7 +186,7 @@ void displayGameStats(int ties, int player1Score, int player2Score) {
 
 	cout << "Player 1's score: " << player1Score << endl;
 	cout << "Player 2's score: " << player2Score << endl;
-	cout << "Total number of ties: " << ties;
+	cout << "Total number of ties: " << ties << endl;
 
 }
 
